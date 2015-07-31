16a0ca618bed1ff09fe93668588bee7ba92c79fc
Nicola Orritos <nicola.orritos@applixgroup.com>
2015-07-31 16:03:29 +0200

    Leveraging tests to make the code much more robust

--- test/all_test.js
+++ test/all_test.js
@@ -8,17 +8,26 @@ var CONF    = require('../lib/conf');
 
 var baseURL = 'http://localhost:' + CONF.PORT + '/well/push';
 
-var doc     = {key: 'value'};
+var doc     = JSON.stringify({key: 'value'});
 
 
 describe('"well" server', function()
 {
     it('must answer to pushes on the default bucket', function(done)
     {
-        request.post(baseURL, {form: doc}, function(err, res)
+        request.post(
+        {
+            url: baseURL,
+            headers:
+            {
+                'Content-Type': 'application/json'
+            },
+            body: doc
+        },
+        function(err, res)
         {
             assert(err == null, 'There was an error pushing to the default bucket. ' + err);
-            assert(res.statusCode === 200);
+            assert(res.statusCode === 200, 'Status code is not 200, but ' + res.statusCode + ' instead');
 
             var result = JSON.parse(res.body);
 
@@ -31,10 +40,39 @@ describe('"well" server', function()
 
     it('must reply with 404 when pushing to non existent buckets', function(done)
     {
-        request.post(baseURL + '/idontexist', doc, function(err, res)
+        request.post(
+        {
+            url: baseURL + '/idontexist',
+            headers:
+            {
+                'Content-Type': 'application/json'
+            },
+            body: doc
+        },
+        function(err, res)
         {
             assert(err == null, 'There was an error connecting to the proxy. ' + err);
             assert(res.statusCode === 404);
+
+            done();
+        });
+    });
+
+    it('must reply with 400 when pushing non-JSON data', function(done)
+    {
+        request.post(
+        {
+            url: baseURL,
+            headers:
+            {
+                'Content-Type': 'application/json'
+            },
+            body: 'I\'m not JSON'
+        },
+        function(err, res)
+        {
+            assert(err == null, 'There was an error connecting to the proxy. ' + err);
+            assert(res.statusCode === 400, 'Status code is not 400, but ' + res.statusCode + ' instead');
 
             done();
         });
